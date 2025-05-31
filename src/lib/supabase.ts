1f91ec2b53a8021a194a626b12c2628a6c096a13
WELLINGTON NASCIMENTO <65622657+Well-SN@users.noreply.github.com>
2025-05-31 13:10:32 -0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 import type { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://ftynjejdzkwokoendsdb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0eW5qZWpkemt3b2tvZW5kc2RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3MDQwMTksImV4cCI6MjA2NDI4MDAxOX0.d3S-6U1ecxVawY5xek3GuIOm4x8Ym2jtj_uF9K9_6yE'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
